916e0cc2b52043274ec7e30e0aa27eb6fce1e5fb
analista3auditoria-svg <analista3.auditoria@casalimpia.com>
2026-08-22 21:23:16 -0500

    APP.py

--- APP.py
+++ APP.py
@@ -12,7 +12,7 @@ warnings.filterwarnings('ignore')
 
 # Configuración de la página
 st.set_page_config(
-    page_title="Auditor-IA Costos Tercerizados TigoUNE",
+    page_title="Auditor",
     page_icon="📋",
     layout="wide"
 )
